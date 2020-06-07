855402cab253e54b8a30ffa8ae4340efec8adc68
Nga Nguyen <ngasdragon@gmail.com>
2020-06-06 21:14:51 -0700

    Installed new packages for navigation

--- screens/home.js
+++ screens/home.js
@@ -1,20 +1,11 @@
 import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
+import { globalStyles } from '../styles/global';
 
 export default Home = () => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.titleText}> Home Screen</Text>
+    <View style={globalStyles.container}>
+      <Text style={globalStyles.titleText}> Home Screen</Text>
     </View>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    padding: 24,
-  },
-  titleText: {
-    fontFamily: 'roboto_bold',
-    fontSize: 18,
-  }
-})
