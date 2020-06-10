2171edb15a272b4fcca24cff5acdbeabd6ecafdd
Nga Nguyen <ngasdragon@gmail.com>
2020-06-09 21:30:11 -0700

    Add images for rating

--- screens/reviewDetail.js
+++ screens/reviewDetail.js
@@ -1,17 +1,24 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
-import { globalStyles } from '../styles/global';
+import { StyleSheet, View, Text, Image } from 'react-native';
+import { globalStyles, images } from '../styles/global';
 import Card from '../shared/card';
 
 export default function ReviewDetails({ route, navigation }) {
-  const { title, rating, body } = route.params.item
+  const { title, rating, body } = route.params.item;
   return (
     <View style={globalStyles.container}>
       <Card>
         <Text>{title}</Text>
-        <Text>{rating}</Text>
         <Text>{body}</Text>
+        <View style={styles.rating}>
+          <Text>GameZone Rating: </Text>
+          <Image source={images.ratings[rating]}/>
+        </View>
       </Card>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+
+})
