16cbe78eb9fd26a2f3a0c1c30356245e7056a516
Nga Nguyen <ngasdragon@gmail.com>
2020-06-09 22:04:58 -0700

    Styled modal

--- screens/home.js
+++ screens/home.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
-import { Text, FlatList, TouchableOpacity } from 'react-native';
+import { StyleSheet, View, Text, FlatList, TouchableOpacity, Modal } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
+
 import { globalStyles } from '../styles/global';
 import Card from '../shared/card';
 import BackgroundImage from '../shared/backgroundImage';
@@ -11,9 +13,26 @@ export default function Home({ navigation }) {
     {title:'Jumazi', rating: 4, body: 'nasd asd', key: '3'},
     {title:'The Last Empress', rating: 3, body: 'ad ae dsfaf', key: '4'},
   ])
-
+  const [modelOpen, setModalOpen] = useState(false);
   return (
     <BackgroundImage>
+      <Modal visible={modelOpen} animationType='slide'>
+        <View style={styles.modalContent}>
+          <MaterialIcons
+            name="close"
+            size={24}
+            style={{...styles.modalToggle, ...styles.modalClose}}
+            onPress={() => setModalOpen(false)}
+          />
+          <Text>Hello</Text>
+        </View>
+      </Modal>
+      <MaterialIcons
+        name="add"
+        size={24}
+        style={styles.modalToggle}
+        onPress={() => setModalOpen(true)}
+      />
       <FlatList
         data={reviews}
         renderItem={({ item }) => (
@@ -27,3 +46,25 @@ export default function Home({ navigation }) {
     </BackgroundImage>
   )
 }
+
+const styles = StyleSheet.create({
+  modalContent: {
+    flex: 1,
+    marginHorizontal: 20,
+    marginVertical: 20,
+    borderRadius: 10,
+    backgroundColor: "#91AA8D"
+  },
+  modalToggle: {
+    marginBottom: 10,
+    borderWidth: 1,
+    borderColor: '#F2F2F2',
+    padding: 10,
+    borderRadius: 10,
+    alignSelf: 'center'
+  },
+  modalClose: {
+    marginTop: 20,
+    marginBottom: 0,
+  }
+})
